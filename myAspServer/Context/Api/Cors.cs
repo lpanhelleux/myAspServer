989516e528fc3d6f355284732d4f0ed6697a74d7
unknown <laurent.panhelleux@gmail.com>
2022-11-19 15:19:06 +0100

    :lock: Authorize Cors

---
+++ myAspServer/Context/Api/Cors.cs
@@ -0,0 +1,22 @@
+﻿namespace myAspServer.Context.Api
+{
+    public static class Cors
+    {
+        public static string Build(WebApplicationBuilder builder)
+        {
+
+            var policy = "_myAllowSpecificOrigins";
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: policy,
+                    policy =>
+                    {
+                        policy.WithOrigins("https://localhost:3000", "http://localhost:3000")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+            return policy;
+        }
+    }
+}
--- myAspServer/Program.cs
+++ myAspServer/Program.cs
@@ -3,6 +3,7 @@ using myAspServer.Context.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var policy =Cors.Build(builder);
 DatabaseBuilder.Build(builder);
 
 var app = builder.Build();
@@ -11,4 +12,5 @@ ApiBuilder.BuildDefaultPageApi().Init(app);
 ApiBuilder.BuildTodoApi().Init(app);
 ApiBuilder.BuildUserApi().Init(app);
 
+app.UseCors(policy);
 app.Run();
